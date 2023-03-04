e119513325414fb780f2f30f55b83c4139a562ac
Nihal Rian Dias <nihaldias261@gmail.com>
2023-03-04 13:53:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 5000
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+  app.get('/users', db.getUsers)
+  app.get('/users/:id', db.getUserById)
+  app.post('/users', db.createUser)
+  app.put('/users/:id', db.updateUser)
+  app.delete('/users/:id', db.deleteUser)
+  
+  app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
